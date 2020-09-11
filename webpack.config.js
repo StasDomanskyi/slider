aec63b796d22ecd24e6805ba25b55cc17841e246
unknown <Stanislav Domanskyi>
2020-09-11 19:39:18 +0300

    set up a webpack

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.txt$/,
+        use: 'raw-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/index.html',
+      
+      inject: 'body',
+    }),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+  ],
+};
